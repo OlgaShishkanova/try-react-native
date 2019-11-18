51458142c055d514510a164823f596aad52dc27a
Olga Shishkanova <olga.shishkanova@statravel.com>
2019-11-18 15:55:27 +0100

    styling lessons

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
-  
-  
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text is changed')}/>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input}/>
+        <Button title="ADD" onPress={() => setOutputText('The text is changed')}/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer:{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'},
+  input: {width: '80%',borderBottomColor: 'black', borderWidth: 1, padding: 10}
 });
