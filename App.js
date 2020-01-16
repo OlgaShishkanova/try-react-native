91d3d28e3e4ba2006456384d0b2f81ccbddd15ec
Olga <lili432@mail.ru>
2020-01-16 19:36:49 +0100

    MealsNavigator + empty screen pages

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MealsNavigator from './navigation/MealsNavigator';
+
+// import { createStackNavigator } from 'react-navigation-stack';
+// import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import { createDrawerNavigator } from 'react-navigation-drawer';
+
+
+// import * as Font from 'expo-font'
+// import {AppLoading} from 'expo';
+
+// const fetchFonts = () => {
+//   Font.loadAsync({
+//     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+//     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+//   })
+// }
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator/>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
